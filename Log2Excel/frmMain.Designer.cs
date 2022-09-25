c08f36dc3fc9bcd94c9d7b24879bfffb2d7efe4b
Pham Quang <quangpv598@gmail.com>
2022-09-26 01:07:08 +0700

    Add project files.

---
+++ Log2Excel/frmMain.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Log2Excel
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOpenConfigFile = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblConvert = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(527, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please configure the software before converting";
+            // 
+            // lblOpenConfigFile
+            // 
+            this.lblOpenConfigFile.AutoSize = true;
+            this.lblOpenConfigFile.Location = new System.Drawing.Point(599, 34);
+            this.lblOpenConfigFile.Name = "lblOpenConfigFile";
+            this.lblOpenConfigFile.Size = new System.Drawing.Size(195, 32);
+            this.lblOpenConfigFile.TabIndex = 3;
+            this.lblOpenConfigFile.TabStop = true;
+            this.lblOpenConfigFile.Text = "Open Config File";
+            this.lblOpenConfigFile.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblOpenConfigFile_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "If you have done,";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(62, 45);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(62, 45);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblConvert
+            // 
+            this.lblConvert.AutoSize = true;
+            this.lblConvert.Location = new System.Drawing.Point(268, 94);
+            this.lblConvert.Name = "lblConvert";
+            this.lblConvert.Size = new System.Drawing.Size(231, 32);
+            this.lblConvert.TabIndex = 7;
+            this.lblConvert.TabStop = true;
+            this.lblConvert.Text = "click here to convert";
+            this.lblConvert.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblConvert_LinkClicked);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 149);
+            this.Controls.Add(this.lblConvert);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblOpenConfigFile);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Convert Log File to Excel File";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label1;
+        private LinkLabel lblOpenConfigFile;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private LinkLabel lblConvert;
+    }
+}
---
+++ Log2Excel/frmMain.cs
@@ -0,0 +1,203 @@
+﻿using ClosedXML;
+using ClosedXML.Excel;
+using ExcelDataReader;
+using System.Collections;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Log2Excel
+{
+    public partial class frmMain : Form
+    {
+        /// <summary>
+        /// Path of config file
+        /// </summary>
+        private readonly string ConfigPath = "Config.xlsx";
+
+        /// <summary>
+        /// Save general environment variables for application
+        /// </summary>
+        private Hashtable htbEnvironmentVariables = new ();
+
+        /// <summary>
+        /// Map command between DZS vs CISCO
+        /// </summary>
+        private List<(string, string)> mapCommands = new ();
+
+        // Các Device Prefix và Suffix sẽ được lưu dưới dạng key trong Dic
+        // Mỗi key sẽ chứa một tập các String
+        public Dictionary<string, Queue<string>> dicDeviceLog = new Dictionary<string, Queue<string>>();
+        public frmMain()
+        {
+            InitializeComponent();
+
+            // Get full config
+            this.ConfigPath = Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), ConfigPath);
+            this.ReadConfigFile();
+        }
+
+        private void ReadConfigFile()
+        {
+            DataSet dataSet = new DataSet();
+
+            using (var stream = File.Open(this.ConfigPath, FileMode.Open, FileAccess.Read))
+            {
+                IExcelDataReader reader;
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                //reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+                dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
+                {
+                    ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                    {
+                        UseHeaderRow = true
+                    }
+                });
+
+                reader.Close();
+
+            }
+
+            // Environment Variables
+            DataTable dtEnvironmentVariables = dataSet.Tables[0];
+            foreach (DataRow dr in dtEnvironmentVariables.Rows)
+            {
+                htbEnvironmentVariables.Add(dr["NAME"], dr["VALUE"]);
+            }
+
+            // DZS vs CISCO map command
+            DataTable dtMapCommand = dataSet.Tables[1];
+            foreach (DataRow dr in dtMapCommand.Rows)
+            {
+                mapCommands.Add((dr["DZS"].ToString(), dr["CISCO"].ToString()));
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty("txtLogPath.Text"))
+            {
+                MessageBox.Show("Please select a log file.");
+                return;
+            }
+
+            //1. Xoá dữ liệu
+            dicDeviceLog.Clear();
+
+            //2. Nạp dữ liệu khởi tạo cho từ điển chứa dữ liệu
+            var deviceSuffix = "txtDeviceSuffix.Text".Split(';');
+            foreach (string suffix in deviceSuffix)
+            {
+                string fullDeviceName = "txtDevicePrefix.Text" + suffix;
+                // Nếu tên thiết bị chưa có trong từ điển, thì thêm vào từ điển
+                if (!dicDeviceLog.ContainsKey(fullDeviceName))
+                {
+                    dicDeviceLog.Add(fullDeviceName, new Queue<string>());
+                }
+            }
+
+            //3. Duyệt các dòng, ghi dữ liệu vào từ điển
+            List<string> lines = new();
+
+            //foreach(var file in fdl.FileNames)
+            //{
+            //    var currentLines = File.ReadAllLines(file);
+
+            //    lines = lines.Concat(currentLines).ToList();
+            //}
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string line = lines[i];
+
+                // Kiểm tra nếu dòng chứa lệnh show
+                if (line.Replace(" ", "").ToLower().Contains("#show"))
+                {
+                    // duyệt qua tên các thiết bị đã lưu trước đó
+                    foreach (string fullDeviceName in dicDeviceLog.Keys)
+                    {
+                        // Nếu dòng mà bắt đầu bằng tên thiết bị
+                        if (line.StartsWith(fullDeviceName))
+                        {
+                            var queueString = dicDeviceLog[fullDeviceName];
+
+                            // Thêm dòng hiện tại
+                            queueString.Enqueue(line);
+
+                            // Thêm các dòng kết quả của lệnh show cho tới khi gặp lại tên thiết bị
+
+                            i++;
+                            line = lines[i];
+                            while (dicDeviceLog.Keys.Where(deviceName => line.StartsWith(deviceName)).Count() == 0)
+                            {
+                                queueString.Enqueue(line);
+                                i++;
+                                line = lines[i];
+                            }
+
+                            // Nếu ngay dòng tiếp theo sau kết quả của lệnh show tiếp đó
+                            // mà là 1 lệnh show mới thì tiến hành trừ i đi 1 để đọc lại đoạn show ở trên
+                            if (line.Replace(" ", "").ToLower().Contains("#show"))
+                            {
+                                i--;
+                            }
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //4. Xuất excel từ bộ dữ liệu từ điển
+            DataTable dtResult = new DataTable();
+            foreach (string columnName in dicDeviceLog.Keys)
+            {
+                dtResult.Columns.Add(columnName, typeof(string));
+            }
+
+            int maxRow = dicDeviceLog.Values.Max(queue => queue.Count);
+            for (int i = 0; i < maxRow; i++)
+            {
+                DataRow drNewRow = dtResult.NewRow();
+
+                foreach (string columnName in dicDeviceLog.Keys)
+                {
+                    var queue = dicDeviceLog[columnName];
+                    if (queue.Count > 0)
+                    {
+                        drNewRow[columnName] = queue.Dequeue();
+                    }
+                }
+
+                dtResult.Rows.Add(drNewRow);
+            }
+
+            string fileName = "";//"txtLogPath.Text".Replace(Path.GetExtension(fdl.FileName), ".xlsx");
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add(dtResult, "Sample Sheet");
+                workbook.SaveAs(fileName);
+            }
+
+            MessageBox.Show($"Convert Success.\nFile : {fileName}");
+        }
+
+        private void lblOpenConfigFile_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo(ConfigPath)
+            {
+                UseShellExecute = true
+            };
+            p.Start();
+        }
+
+        private void lblConvert_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+    }
+}
